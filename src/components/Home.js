74f7d7017f898aa2b4dc3da1f8c0085498173fbc
Debrah Navajjah Muinde <debbynav645@gmail.com>
2024-11-15 00:18:02 +0300

    Modifies files

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import backgroundvid from '../Assets/Videos/video1.mp4'
 import './Home.css'
 import Player from './Player'
+import AddSongForm from './AddSongForm'
+import Sidebar from './Sidebar'
 // import Search from './Search'
 
 
 
 function Home() {
+  const [showAddSongForm, setShowAddSongForm] = useState(false)
+
+  const toggleAddSongForm = () => {
+    setShowAddSongForm(!showAddSongForm)
+  }
+
 return (
 <div className='container'>
   <video src={backgroundvid} autoPlay muted loop className='backgroundVideo'></video>
   <div className='blankScreen'></div> 
+  <Sidebar toggleAddSongForm={toggleAddSongForm} />
   {/* <Search /> */}
   <Player />
+  {showAddSongForm && <AddSongForm />}
   </div>
 
  
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,16 +3,19 @@ import home from '../Assets/icons/home.svg'
 import librarymusic from '../Assets/icons/library_music.svg'
 import playlistadd from '../Assets/icons/playlist_add.svg'
 import playarrow from '../Assets/icons/play_arrow.svg'
+import addbtn from '../Assets/icons/add.svg'
+import pause from '../Assets/icons/pause.svg'
+import tunesettings from '../Assets/icons/tune_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg'
 import './Sidebar.css'
 
 
-function Sidebar() {
-    const [isOpen, setIsOpen] = useState(false);
+function Sidebar({ toggleAddSongForm }) {
+    const [isOpen, setIsOpen] = useState(false)
 
   const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
-
+    setIsOpen(!isOpen)
+  }
+ 
 
     
   return (
@@ -24,7 +27,10 @@ function Sidebar() {
         <li><img src={home} alt='home'/></li>
         <li><img src={librarymusic} alt='music library'/></li>
         <li><img src={playlistadd} alt='add playlist'/></li>
+        <li><img src={addbtn} alt='add' onClick={toggleAddSongForm}/></li>
         <li><img src={playarrow} alt='play'/></li>
+        <li><img src={pause} alt='pause'/></li>
+        <li><img src={tunesettings} alt='tune settings'/></li>
       </ul>
     </div>
   )
