f0ee5946a5dac5e4d9168cf8ba398e7663971013
Debrah Navajjah Muinde <debbynav645@gmail.com>
2024-11-15 02:09:15 +0300

    Adds search feature

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import search from '../Assets/icons/search_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg'
 
-function Search() {
+
+function Search({ onSearch }) {
+    const [inquiry, setInquiry] = useState('')
+
+    const handleInputChange = (e) => {
+        setInquiry(e.target.value)
+        onSearch(e.target.value)
+    }
   return (
-    <div>
+    <div className='search-container'>
         <input
         type="text"
-        placeholder="Search by title, artist, or album"
+        placeholder="Search for a song..."
+        value={inquiry}
+        onChange={handleInputChange}
+        className='search-input'
         />
+        <button onClick={() => onSearch(inquiry)} className='searchbtn'> {search} </button>
     </div>
   )
 }
